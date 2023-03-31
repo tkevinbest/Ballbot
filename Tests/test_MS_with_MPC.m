343b68ef5a8d524be45e0e472ff9281efd8f23f6
Kevin Best <tkevinbest@gmail.com>
2023-03-30 21:27:36 -0400

    Updated MS/MPC code to match.

--- Tests/test_MS_with_MPC.m
+++ Tests/test_MS_with_MPC.m
@@ -4,7 +4,7 @@ close all
 [mk, mw, ma, rk, rw, ra,l, omegaK, omegaW, omegaA, g] = Ballbot.defineParams();
 
 % Desired X position
-desX = 1.25;
+desX = 1.0;
 
 % Number of nodes
 N = 26; 
@@ -39,8 +39,8 @@ zstar = interleave2(qstar, qdotstar, 'row');
 
 %% Run MPC around nominal trajectory
 % Configure controller
-dt = 0.025; % Real time sample rate
-timeHorizon = 2; 
+dt = 0.05; % Real time sample rate
+timeHorizon = 3; 
 
 % Configure simulation
 T_simulation = max(tstar); % Length of the simulation
@@ -52,39 +52,49 @@ zstarTraj = interp1(tstar', zstar', t_sim')';
 ustarTraj = interp1(tstar', ustar', t_sim')'; 
 
 % Configure MPC 
-MPCconfig = Control.MPC.setup(dt, timeHorizon, ...
-    zstarTraj, ustarTraj, t_sim);
+MPCconfig = Control.MPC.setup(dt, timeHorizon, t_sim, z0);
+
+% Add horizon's length buffer to the end of the trajectory
+zstarTraj = [zstarTraj, repmat(zstarTraj(:,end),1,MPCconfig.N_horizon)];
+ustarTraj = [ustarTraj, repmat(ustarTraj(:,end),1,MPCconfig.N_horizon)];
 
 % Allocate space to save off the trajectory
 u_store = zeros(1, length(t_sim)); 
 z_store = zeros(4, length(t_sim)); 
+mpcTime = zeros(length(t_sim),1); 
 
 % Loop simulation
+curZ = z0; 
 for ix = 1:length(t_sim)
     % Get the current time
     curTime = t_sim(ix); 
 
-    % Calculate deviation from nominal states
-    ztilde = z0 - zstarTraj(:,ix);
+    % Grab current section of desired trajectory
+    % For a constant setpoint, just repeat desired 
+
+    zDesiredTraj_thisHorizon = zstarTraj(:,ix:ix+MPCconfig.N_horizon-1); 
+    uDesiredTraj_thisHorizon = ustarTraj(:,ix:ix+MPCconfig.N_horizon-1); 
 
     % Solve optimal control
-    Q = diag([10, 1, .5,.5])*eye(4); 
+    Q = diag([1000, 1, 100,.5]); 
     R = 1; 
-    tic;
-    u = Control.MPC.run(Q, R, ztilde, curTime, MPCconfig); 
+    tic
+    [curU, zstar, ustar, MPCconfig] = Control.MPC.run(Q, R, curZ, ...
+                                                        zDesiredTraj_thisHorizon, ...
+                                                        uDesiredTraj_thisHorizon, MPCconfig); 
     mpcTime(ix) = toc;
 
     % Run simulation with said control 
-    forceFunc = @(t,z) u + ustarTraj(ix); 
-    q0 = z0([1,3],:);
-    qdot0 = z0([2,4],:);
+    forceFunc = @(t,z) curU; %*(rand()*.2 + .9); 
+    q0 = curZ([1,3],:);
+    qdot0 = curZ([2,4],:);
     [t,q, qdot, z, ~] = Ballbot.runSimulation(q0, qdot0, forceFunc, curTime + [0,dt], false);
 
     % Update initial conditions
-    z0 = z(end,:)';
+    curZ = z(end,:)';
 
     % Store the trajectories
-    u_store(ix) = u + ustarTraj(ix); 
+    u_store(ix) = curU; 
     z_store(:,ix) = z(1,:)'; 
 end
 
