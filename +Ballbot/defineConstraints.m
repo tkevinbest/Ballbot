a0513c0578c16823fe68024b1f654fc201985ba6
Kevin Best <tkevinbest@gmail.com>
2023-04-25 21:22:42 -0400

    Update defineConstraints.m

--- +Ballbot/defineConstraints.m
+++ +Ballbot/defineConstraints.m
@@ -7,21 +7,21 @@ switch type
                 -1/rk, 1.5/rk;
                 -deg2rad(30), deg2rad(30)
             ];
-        qdotLim = 5*[
-                -15, 15;
-                -2, 2
+        qdotLim = [
+                -75, 75;
+                -10, 10
             ];
         uLim = [-10, 10]; 
     case Control.OptimizationType.MPC
         qLim = [
                 -3/rk, 3/rk;
-                -deg2rad(90), deg2rad(90)
+                -deg2rad(45), deg2rad(45)
             ];
-        qdotLim = [
-                -500, 500;
-                -20, 20
+        qdotLim = 1.5*[
+                -75, 75;
+                -10, 10
             ];
-        uLim = 20*[-20, 20]; 
+        uLim = 5*[-10, 10]; 
 end
 
 end
